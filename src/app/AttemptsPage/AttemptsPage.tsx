8e0b890c08448058b7bf6b211a9d5a86511728cc
Wirtrax <ngrbuster@gmail.com>
2026-08-26 20:08:05 +0300

    feat: added attempt and attempts page

---
+++ src/app/AttemptsPage/AttemptsPage.tsx
@@ -0,0 +1,119 @@
+import { useEffect, useState, type ChangeEvent } from 'react';
+import Input from '../../components/Input/AdminInput';
+import Table from '../../components/Table/Table';
+import Title from '../../components/Title/Title';
+import StatusBadge from '../../components/StatusBadge/StatusBadge';
+import { getAttempts } from '../../service/api';
+import type { TableColumn } from '../../components/Table/tableProps';
+import s from './AttemptsPage.module.scss';
+import pageStyle from '../Page.module.scss';
+import { useNavigate } from 'react-router-dom';
+import debounce from '../../utils/debounse';
+import type { AttemptsType } from '../../types/apiType';
+import { getFirstLetters } from '../../utils/firstLetters';
+import { generateBlueGray } from '../../utils/generateBlueGray';
+import Coin from '../../assets/ico/interface/currency.svg?react';
+
+function AttemptsPage() {
+  const [attempts, setAttempts] = useState<AttemptsType[]>([]);
+  const [searchValue, setSearchValue] = useState<string>('');
+  const [debouncedSearchValue, setDebouncedSearchValue] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchattempts = async () => {
+      setLoading(true);
+      try {
+        const data = await getAttempts();
+        console.log(data);
+        setAttempts(data);
+      } catch (error) {
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchattempts();
+  }, []);
+
+  const debouncedSetSearch = debounce((value: string) => {
+    setDebouncedSearchValue(value);
+  }, 300);
+
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>): void => {
+    const value = event.target.value;
+    setSearchValue(value);
+    debouncedSetSearch(value);
+  };
+
+  const filteredAttempts = attempts.filter((item) =>
+    item.full_name.toLocaleLowerCase().includes(debouncedSearchValue.toLocaleLowerCase())
+  );
+
+  const columns: TableColumn<AttemptsType>[] = [
+    {
+      key: 'full_name',
+      title: 'Участник',
+      render: (_, item) => {
+        return (
+          <span className={s['table__name']}>
+            <span style={{ background: generateBlueGray() }} className={s['table__name-initial']}>
+              {getFirstLetters(`${item.full_name}`, 2)}
+            </span>
+            <span className={s['table__full-name']}>{item.full_name}</span>
+          </span>
+        );
+      },
+    },
+    {
+      key: 'activity',
+      title: 'Активность',
+    },
+    {
+      key: 'status',
+      title: 'Статус',
+      render: (_, item) => {
+        return (
+          <StatusBadge
+            variant={item.status == 'accepted' ? 'received' : item.status == 'declined' ? 'cancelled' : 'pending'}
+          />
+        );
+      },
+    },
+    {
+      key: 'reward',
+      title: 'Награда',
+      render: (_, item) => {
+        return (
+          <span className={s['table__reward']}>
+            +{item.reward} <Coin />
+          </span>
+        );
+      },
+    },
+  ];
+
+  return (
+    <section>
+      <div className={pageStyle['header']}>
+        <Title
+          title="Участники игр"
+          subtitle="Попытки прохождения активностей — нажмите для просмотра результата игры"
+        />
+        <div className={pageStyle['search']}>
+          <Input placeholder="поиск по названию" type="search" value={searchValue} onChange={handleSearch} />
+        </div>
+      </div>
+      <Table
+        title="Каталог"
+        countElements={`${attempts.length} позиций`}
+        columns={columns}
+        data={filteredAttempts}
+        link={(attempts) => `/admin/attempts/${attempts.id}`}
+      />
+    </section>
+  );
+}
+
+export default AttemptsPage;
